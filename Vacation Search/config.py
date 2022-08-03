e53a0a386d0811cdbe63cf6e863a37541502bfd9
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-08-03 15:43:57 -0700

    Vacation Itinerary Completed

---
+++ Vacation Search/config.py
@@ -0,0 +1,5 @@
+weather_api_key="257b844c0cc5720c680b7f610db0f588"
+def make_quesadilla(protein, topping): 
+    quesadilla - f"Here is a {protein} quesadilla with{topping}"
+    print(quesadilla)
+g_key="AIzaSyBD_GoQswZaXcB0HEJBgVE9jRjIEbo7X78"
---
+++ Vacation_Itinerary/config.py
@@ -0,0 +1,6 @@
+weather_api_key="257b844c0cc5720c680b7f610db0f588"
+def make_quesadilla(protein, topping): 
+    quesadilla - f"Here is a {protein} quesadilla with{topping}"
+    print(quesadilla)
+    # Google API Key
+g_key = "AIzaSyC0sFNwD5wHqedDPyVnjEHWUI3u5nvKmq0"
