97638907f1a63b806bfb160ce9407600990edeb8
Richard Pamintuan <106283411+rpamintuan671@users.noreply.github.com>
2022-08-04 08:46:19 -0700

    Delete config.py

--- Weather_Database/config.py
+++
@@ -1,5 +0,0 @@
-weather_api_key="257b844c0cc5720c680b7f610db0f588"
-def make_quesadilla(protein, topping): 
-    quesadilla - f"Here is a {protein} quesadilla with{topping}"
-    print(quesadilla)
-g_key="AIzaSyBD_GoQswZaXcB0HEJBgVE9jRjIEbo7X78"
